928ccade5b78e51799a1ebd602d5fd01b08beb60
Alexander Danowicz <alexdanowicz@gmail.com>
2020-07-12 18:47:42 -0300

    Coding challenge finalized

--- 01-fundamentals/01-rgb-color-visualizer/script.js
+++ 01-fundamentals/01-rgb-color-visualizer/script.js
@@ -2,28 +2,27 @@ const ranges = document.querySelectorAll('.range');
 const counters = document.querySelectorAll('.counter');
 const color = document.querySelector('.color');
 
-let red, green, blue = 0;
+let red = 0;
+let green = 0;
+let blue = 0;
 
-
-
-for (let range of ranges) {
+for (const range of ranges) {
     range.addEventListener('input', (e) => {
-        const targetName = range.name;
-        
-        for(let counter of counters) {
-            if (counter.name === targetName) counter.value = range.value;
-            switch (targetName) {
-                case 'red':
-                    red = range.value;
-                    break;
-                case 'green':
-                    green = counter.value;
-                    break;
-                case 'blue':
-                    blue = counter.value;
-                    break;
-            }
-            
+        const target = e.target;
+        switch (target.name) {
+            case 'red':
+                red = target.value;
+                break;
+            case 'green':
+                green = target.value;
+                break;
+            case 'blue':
+                blue = target.value;
+                break;
+        }
+        color.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+        for(const counter of counters) {
+            if (counter.name === target.name) counter.value = range.value;            
         }
     })
 }
